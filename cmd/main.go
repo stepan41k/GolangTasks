13b75d8028b85615baa849f761acb59f1dc73bb0
stepan41k <russia.zont@gmail.com>
2025-11-18 00:12:24 +0300

    fix: rename to pipeline

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-func GoroutinesPrint(n int) {
-    fmt.Printf("Goroitine %d\n", n)
-}
- 
-func main() {
-	wg := &sync.WaitGroup{}
 
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			GoroutinesPrint(i)
-		}(i)	
+func main() {
+	// done := make(chan struct{})
+
+	// for i := 0; i < 5; i++ {
+    // 	go func(i int) {
+    //     	fmt.Println("work", i)
+    //     	done <- struct{}{}
+    // 	}(i)
+
+    // <-done // ждём завершения горутины i
+	// }
+
+	n := 5
+	signals := make([]chan struct{}, n)
+
+	for i := 0; i < n; i++ {
+		signals[i] = make(chan struct{})
 	}
 
-	wg.Wait()
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			if i > 0 {
+				<-signals[i-1]
+			}
+			fmt.Println("work", i)
+			close(signals[i])
+		}(i)
+	}
+
+	<-signals[n-1]
 }
