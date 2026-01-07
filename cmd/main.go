70fed86ced0a35cc8d54e753c4bea0c021f76ac6
stepna41k <russia.zont@gmail.com>
2026-01-07 19:06:59 +0300

    ?

--- cmd/main.go
+++ cmd/main.go
@@ -2,47 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
-
-var (
-	countJobs = 5
-	countWorkers = 3
-)
-
-func merge(jobs chan int, result chan int) {
-	for v := range jobs {
-		result <- v * v
-	}
-}
 
 func main() {
-	jobsChan := make(chan int)
-	resultChan := make(chan int)
-
-	go func() {
-		for i := 1; i <= countJobs; i++ {
-			jobsChan <- i
-		}
-
-		close(jobsChan)
-	}()
-
-	wg := &sync.WaitGroup{}
-	wg.Add(countWorkers)
-	for i := 0; i < countWorkers; i++ {
-		go func() {
-			defer wg.Done()
-			merge(jobsChan, resultChan)
-		}()
-	}
-
-	go func() {
-		wg.Wait()
-		close(resultChan)
-	}()
-
-	for v := range resultChan {
-		fmt.Println(v)
-	}
+	fmt.Println("ok")
 }
