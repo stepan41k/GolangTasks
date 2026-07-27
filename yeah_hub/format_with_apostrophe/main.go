7b23aa951d63151b643b961a426b712daef094de
stepan41k <russia.zont@gmail.com>
2026-07-27 23:14:40 +0300

    fix(yeah_hub): update solution for format with apostrophe task

--- yeah_hub/format_with_apostrophe/main.go
+++ yeah_hub/format_with_apostrophe/main.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"strconv"
 	"strings"
 )
 
 func FormatWithApostrophe(price int) string {
-	if price <= 999 {
-		fmt.Println(price)
+	s := strconv.Itoa(price)
+	n := len(s)
+	
+	if n <= 3 {
+		return s
 	}
 	
 	res := strings.Builder{}
 	
-	for price > 0 {
-		res.WriteString(strconv.FormatInt(int64(price % 1000), 10))
-		res.WriteRune(' ')
-		price /= 1000
+	for i, char := range s {
+		if i > 0 && (n-i) % 3 == 0 {
+			res.WriteByte('\'')
+		}
+		res.WriteRune(char)
 	}
-
-	res2 := strings.Split(res.String(), " ")
-	slices.Reverse(res2)
-	res3 := strings.Join(res2[1:], " ")
 	
-	return res3
+	return res.String()
 }
 
 func main() {
