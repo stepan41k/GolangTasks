af4e20a9ddfacff16149c3cff9adc48431b90519
stepan41k <russia.zont@gmail.com>
2026-07-30 23:34:10 +0300

    feat: convert int to string with one cycle

---
+++ yeah_hub/convert_to_string/one_range.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 156
+
+	var runes [11]rune
+	pos := len(runes) - 1 
+
+	for x > 0 {
+		digit := x % 10
+		runes[pos] = '0' + rune(digit)
+		pos--
+		x /= 10
+	}
+		
+	fmt.Println(string(runes[:]))
+}
