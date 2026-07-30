d8c7214ef9426f015f4f43d69f6921534a1e0d0e
stepan41k <russia.zont@gmail.com>
2026-07-30 23:33:59 +0300

    feat: convert int to string with two cycles

---
+++ yeah_hub/convert_to_string/two_range.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 156
+
+	var runes []rune
+
+	for x > 0 {
+		digit := x % 10
+		runes = append(runes, '0' + rune(digit))
+		x /= 10
+	}
+
+	for i, j := 0, len(runes) - 1; i < j; i, j = i + 1, j - 1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+		
+	fmt.Println(string(runes))
+}
