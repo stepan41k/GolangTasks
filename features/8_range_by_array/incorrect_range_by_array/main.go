f7e876c6b3b4bdc0af81b3a2c0d20cb77a8b0571
stepan41k <russia.zont@gmail.com>
2026-08-08 23:19:04 +0300

    feat(features): add incorrect range by array

---
+++ features/8_range_by_array/incorrect_range_by_array/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	values := [...]int{100, 200, 300}
+
+	for idx, value := range values {
+		value += 50
+		fmt.Println("#1:", unsafe.Pointer(&value), "#2:", unsafe.Pointer(&values[idx]))
+	}
+
+	fmt.Println(values)
+}
